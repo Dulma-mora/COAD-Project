7a03e0249d3e21e01ed1483087783d04d9e02cbd
María Montero <60896899+Dulma-mora@users.noreply.github.com>
2022-02-02 11:21:42 -0600

    Update 5.density_plot.R

--- Scripts/5.density_plot.R
+++ Scripts/5.density_plot.R
@@ -1,7 +1,7 @@
 #### density plot
 library("readr")
 library("dplyr")
-
+library("igraph")
 
 # paths
 
@@ -13,6 +13,7 @@ path_mir <- "COAD_mi.tsv"
 coad_mi <- as.data.frame(readr::read_tsv(path_mir)) # wow!
 
 coad_mi <- read.table(file = 'COAD_mi.tsv', sep = '\t', header = TRUE)
+
 
 #### nueva matriz de adyacencia 
 
@@ -26,7 +27,7 @@ y <- ifelse(coad_mi < quantile(coad_mi, 0.1), 0, 1)
 
 
 
-# descubriendo que hace esto
+# descubriendo que hace esto lol
 
 g <- igraph::graph_from_incidence_matrix(y)
 
